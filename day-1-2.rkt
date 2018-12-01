41657ca6fe5c8b7d52e006ffe7225ba0076666c1
Bogdan Popa <popa.bogdanp@gmail.com>
2018-12-01 07:23:11 +0200

    chore: day 1

---
+++ day-1-2.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(let outer ([freq 0]
+            [seen (set)])
+  (call-with-input-file "day-1-2.txt"
+    (lambda (in)
+      (let loop ([n (read in)]
+                 [freq freq]
+                 [seen seen])
+        (cond
+          [(eof-object? n)
+           (outer freq seen)]
+
+          [(set-member? seen freq) freq]
+
+          [(loop (read in) (+ freq n) (set-add seen freq))])))))
---
+++ day-1.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(call-with-input-file "day-1.txt"
+  (lambda (in)
+    (let loop ([n (read in)]
+               [freq 0])
+      (cond
+        [(eof-object? n) freq]
+        [(loop (read in) (+ freq n))]))))
